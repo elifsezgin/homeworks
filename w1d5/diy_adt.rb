8b1d7fd64ab66860a1715bce39f66a4a5dc61108
Elif Sezgin <elifmsezgin@gmail.com>
2016-11-18 01:35:19 -0800

    created queue class

--- w1d5/diy_adt.rb
+++ w1d5/diy_adt.rb
@@ -1,4 +1,5 @@
 class Stack
+
   def initialize
     @ivar = []
     # create ivar to store stack here!
@@ -18,18 +19,47 @@ class Stack
     @ivar.dup
     # return a copy of the stack
   end
+
+end
+
+class Queue
+
+  def initialize
+    @ivar = []
+  end
+
+  def enqueue(el)
+    @ivar.unshift(el)
+  end
+
+  def dequeue
+    @ivar.pop
+  end
+
+  def show
+    @ivar.dup
+  end
+
 end
 
 
 #test codes
 if __FILE__ == $PROGRAM_NAME
-  stack = Stack.new
-  stack.add(1)
-  stack.show
-  stack.add(2)
-  stack.show
-  stack.add(4)
-  stack.show
-  stack.remove
-  stack.show
+  p stack = Stack.new
+  p stack.add(1)
+  p stack.show
+  p stack.add(2)
+  p stack.show
+  p stack.add(4)
+  p stack.show
+  p stack.remove
+  p stack.show
+
+  p queue = Queue.new
+  p queue.enqueue(1)
+  p queue.show
+  p queue.enqueue(4)
+  p queue.show
+  p queue.dequeue
+  p queue.show
 end
